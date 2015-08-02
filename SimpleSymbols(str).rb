36d0b46127f907e879ef08a212583a43110c6ad0
Dwannnn <ehghks27@snu.ac.kr>
2015-08-02 10:00:25 +0900

    Create SimpleSymbols(str).rb

---
+++ SimpleSymbols(str).rb
@@ -0,0 +1,10 @@
+def SimpleSymbols(str)
+
+  # code goes here
+  return str == "Argument " + "goes " + "here"
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleSymbols(STDIN.gets)           
